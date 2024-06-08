a98ebf4c1da5c8fd93054c2bfcfbff3b3b71970c
Ziyang Yu <2524621739@qq.com>
2024-06-08 20:24:04 +0900

    cuda working on

--- 09_io/12_derived_write.cpp
+++ 09_io/12_derived_write.cpp
@@ -12,12 +12,17 @@ int main(int argc, char** argv) {
   MPI_File file;
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
-  MPI_Comm_rank(MPI_COMM_WORLD, &mpirank);
+  MPI_Comm_rank(MPI_COMM_WORLD, &mpirank); 
+  // The code only works for 4 process
   assert(mpisize == dim[0]*dim[1]);
+  // N = {10000, 10000}
   int N[2] = {NX, NY};
+  // Nlocal = {5000, 5000}, 移动的距离 
   int Nlocal[2] = {NX/dim[0], NY/dim[1]};
+  //偏置, {进程号与维度取除，取余} 每个rank对应处理一个数据块
   int offset[2] = {mpirank / dim[0], mpirank % dim[0]};
   for(int i=0; i<2; i++) offset[i] *= Nlocal[i];
+  // buffer is located for local sub matrix
   vector<int> buffer(Nlocal[0]*Nlocal[1],mpirank);
   MPI_Datatype MPI_SUBARRAY;
   MPI_Type_create_subarray(2, N, Nlocal, offset,
